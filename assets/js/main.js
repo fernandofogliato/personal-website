30a42379e838002e6c06705185e6a9dbb7b2314f
Fernando Fogliato <fernandofogliato@gmail.com>
2020-11-20 20:51:36 -0300

    Initial import

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+$(window).on('load', function() {
+
+    $('.level-bar-inner').each(function() {
+    
+        var itemWidth = $(this).data('level');
+        
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+        
+    });
+
+});
+
+
+jQuery(document).ready(function($) {
+    $('.level-bar-inner').css('width', '0');
+    
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+    
+    /* Github Calendar - https://github.com/IonicaBizau/github-calendar */
+    new GitHubCalendar("#github-graph", "fernandofogliato", { responsive: true });
+
+});
